219d564d6d537bfe8731a8be1d84ace093124f7b
Benjamin Mundama <benjamin.mundama@strathmore.edu>
2024-06-14 10:30:19 +0300

    updated form v2

--- contacts.php
+++ contacts.php
@@ -3,12 +3,12 @@
 <?php include_once ("templates/heading.php"); ?>
 <?php include_once ("templates/nav.php"); ?>
 <div class="banner">
-    <h1>Banner</h1> 
+    <h1>Talk to Us</h1> 
 </div>
 
 <div class="row">
     <div class="content">
-    <h1>Forms</h1>
+    <h1>Talk to Us</h1>
     <form action="" method="post" class="contacts_form">
 
         <label for="fn">Fullname:</label><br>
@@ -16,9 +16,6 @@
 
         <label for="em">Email Address:</label><br>
         <input type="email" id="em" placeholder="Email Address" name="em"><br><br>
-            
-
-        
         <label for="sb">Subject:</label><br>
         <select name="subject_line" id="sb" required>
             <option value="">---Select Subject-</option>
@@ -26,16 +23,11 @@
             <option value="eLearning Support">eLearning Support</option>
             <option value="AMS Support">AMS Support</option>
         </select>
-
         <br><br>
-        
         <label for="sb">Message:</label><br>
-            <textarea name="" id="" cols="30" rows="5"></textarea>
-
+        <textarea name="" id="" cols="30" rows="5"></textarea>
             <br><br>
-
-            <input type="submit" value="Send Message">
-
+        <input type="submit" value="Send Message">
                 </div>
 <?php include_once ("templates/side_bar.php"); ?>
         </div>
